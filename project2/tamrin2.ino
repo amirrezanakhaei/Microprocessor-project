5f97981edc0280bd327feb9d6cff5d5db4e37308
amirrezanakhaei <amirrezaavm1.3.8.6@gmail.com>
2025-10-20 21:15:42 +0330

    Arduino file

---
+++ project2/tamrin2.ino
@@ -0,0 +1,18 @@
+const int ledPin = 9;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(Serial.available()>0){
+    char incomingByte = Serial.read();
+    if(incomingByte =='H'){
+      digitalWrite(ledPin, HIGH);
+    }else if(incomingByte == 'L'){
+      digitalWrite(ledPin, LOW);
+    }
+  }
+}
+
