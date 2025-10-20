a2c9d488248e0d47e428e30febe3b69a52690720
amirrezanakhaei <amirrezaavm1.3.8.6@gmail.com>
2025-10-20 21:29:18 +0330

    Arduino file

---
+++ project5/tamrin5.ino
@@ -0,0 +1,22 @@
+const int ledPin = 13;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    String command = Serial.readStringUntil('\n');
+    int value = command.toInt();
+    if (command == "SABZ") {
+      digitalWrite(ledPin, HIGH);
+      Serial.println("LED ON");
+    } else if (value == 50) {
+      digitalWrite(ledPin, LOW);
+      Serial.println("LED OFF");
+    } else {
+      Serial.println("invalid cammand");
+    }
+  }
+}
