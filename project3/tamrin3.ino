a52f4c2d81686f5921fb3405d0d17ff05dc396ed
amirrezanakhaei <amirrezaavm1.3.8.6@gmail.com>
2025-10-20 21:23:24 +0330

    Arduino file

---
+++ project3/tamrin3.ino
@@ -0,0 +1,17 @@
+const int ledPin = 13;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    String command = Serial.readStringUntil('\n');
+    if (command == "ROSHAN") {
+      digitalWrite(ledPin, HIGH);
+    } else if (command == "KHAMOOSH") {
+      digitalWrite(ledPin, LOW);
+    }
+  }
+}
